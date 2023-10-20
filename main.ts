48da259a0c3affd4c5690417f360928514765ccb
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-20 13:20:21 -0400

    making code

--- main.ts
+++ main.ts
@@ -5,6 +5,13 @@
  * This program campare to random numbers
 */
 
+// variables
+let randomNumber1: number
+
 // setup
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    
+})
