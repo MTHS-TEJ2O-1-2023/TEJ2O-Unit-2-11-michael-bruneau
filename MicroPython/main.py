cb6f2ccd92045cbc6cfaf0bf895f85ad1a5ab67f
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-24 13:30:10 +0000

    save

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,39 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Michael Bruneau
+Created on: Oct 2023
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+import random
 
+random_number1 = -1
+random_number2 = -1
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        display.clear()
+        random_number1 = random.randint(0, 99)
+        display.scroll(("#1 :") + str(random_number1))
+        sleep(1000)
+        display.show(Image.HAPPY)
+
+    if button_b.is_pressed():
+        display.clear()
+        random_number2 = random.randint(0, 99)
+        display.scroll(("#2 :") + str(random_number2))
+        sleep(1000)
+        display.show(Image.HAPPY)
+
+    if accelerometer.was_gesture('shake'):
+        display.clear()
+        if random_number1 == -1 & random_number2 == -1:
+            display.scroll("numbers not picked")
+        else:
+            if random_number1 > random_number2:
+                display.scroll(str(random_number1) + (">") + str(random_number2))
+            else:
+                display.scroll(str(random_number1) + ("<") + str(random_number2))
