2d7ad59a301ff0ce13b5c59dac5ba69ebe0657b4
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-10-25 16:51:21 +0000

    python final version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,15 +7,20 @@ This module is a Micro:bit MicroPython program
 from microbit import *
 import random
 
+# variables
 random_number1 = -1
 random_number2 = -1
 
+# setup
 display.clear()
 display.show(Image.HAPPY)
 
+# loop
 while True:
     if button_a.is_pressed():
         display.clear()
+
+        # picking random number 1
         random_number1 = random.randint(0, 99)
         display.scroll(("#1 : ") + str(random_number1))
         sleep(1000)
@@ -23,6 +28,8 @@ while True:
 
     if button_b.is_pressed():
         display.clear()
+
+        # picking random number 2
         random_number2 = random.randint(0, 99)
         display.scroll(("#2 : ") + str(random_number2))
         sleep(1000)
@@ -30,9 +37,12 @@ while True:
 
     if accelerometer.was_gesture("shake"):
         display.clear()
+
+        # making sure random numbers are picked
         if random_number1 == -1 or random_number2 == -1:
             display.scroll("numbers not picked")
         else:
+            # camparing the random numbers
             if random_number1 > random_number2:
                 display.scroll(str(random_number1) + (" > ") + str(random_number2))
             else:
